2446bd4f46e4d625239e608cede45e97fe1e079e
Harper5-1-crypto <lihabo319@gmail.com>
2025-12-05 21:34:50 +0800

    Site updated: 2025-12-05 21:34:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/04/思考/2/","2025/12/01/思考/1/"];function toRandomPost(){
+var posts=["2025/12/01/思考/1/","2025/12/04/思考/2/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
